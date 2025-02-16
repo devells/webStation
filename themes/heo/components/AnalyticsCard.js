33301f6c116633f2009fa57f2e3e8cdd78c0c48f
wanghaozhe <wanghaozhe@sttun.com>
2025-02-16 17:18:37 +0800

    *

--- themes/heo/components/AnalyticsCard.js
+++ themes/heo/components/AnalyticsCard.js
@@ -1,4 +1,4 @@
-import CONFIG from '../config'
+    import CONFIG from '../config'
 import { siteConfig } from '@/lib/config'
 
 /**
--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,11 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2022-06-04', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.devells-art.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -53,7 +52,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://www.devells-art.com',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
